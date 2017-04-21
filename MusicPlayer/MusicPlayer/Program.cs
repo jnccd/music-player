5e4471cfc8a0468a573b7e1a3bf5a8bdd1bc060a
jnccd <kobidogao@outlook.com>
2017-04-21 19:41:29 +0200

    Changes

--- MusicPlayer/MusicPlayer/Program.cs
+++ MusicPlayer/MusicPlayer/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MusicPlayerwNAudio;
 
 namespace MusicPlayer
 {
@@ -20,15 +21,13 @@ namespace MusicPlayer
             Console.WriteLine("Created with \"Microsoft XNA Game Studio 4.0\" and \"NAudio\"");
             foreach (Process p in Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName))
                 if (p.Id != Process.GetCurrentProcess().Id)
-                { p.Kill(); Console.WriteLine("Killed another instance of this program!"); }
-
+                { p.CloseMainWindow(); Console.WriteLine("Killed another instance of this program!"); Thread.Sleep(500); config.Default.Reload(); }
+            
             Program.args = args;
 
             InterceptKeys._hookID = InterceptKeys.SetHook(InterceptKeys._proc);
             using (XNA game = new XNA())
                 game.Run();
-            InterceptKeys.UnhookWindowsHookEx(InterceptKeys._hookID);
-            Assets.DisposeNAudioData();
         }
     }
 }
