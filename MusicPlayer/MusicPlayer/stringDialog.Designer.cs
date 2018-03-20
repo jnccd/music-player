6e1920922a4747cb08503dfa760e913e1de93a20
Niklas Carstensen <creeper99.mc@gmail.com>
2018-03-20 18:34:43 +0100

    The youtube song downloader now also automaticly adds mp3 metadata tags

---
+++ MusicPlayer/MusicPlayer/stringDialog.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MusicPlayer
+{
+    partial class stringDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.done = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // done
+            // 
+            this.done.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.done.Location = new System.Drawing.Point(205, 10);
+            this.done.Name = "done";
+            this.done.Size = new System.Drawing.Size(75, 23);
+            this.done.TabIndex = 1;
+            this.done.Text = "Done";
+            this.done.UseVisualStyleBackColor = true;
+            this.done.Click += new System.EventHandler(this.done_Click);
+            // 
+            // stringDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 43);
+            this.Controls.Add(this.done);
+            this.Controls.Add(this.textBox1);
+            this.Name = "stringDialog";
+            this.Text = "Question";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button done;
+    }
+}
---
+++ MusicPlayer/MusicPlayer/stringDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class stringDialog : Form
+    {
+        public string result;
+
+        public stringDialog(string Question, string preText)
+        {
+            InitializeComponent();
+            Text = Question;
+            textBox1.Text = preText;
+        }
+
+        private void done_Click(object sender, EventArgs e)
+        {
+            result = textBox1.Text;
+            Close();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\n')
+                done_Click(this, EventArgs.Empty);
+        }
+    }
+}
