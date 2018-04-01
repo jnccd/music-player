eea740e8ca5edcd0b150c49c585dd4baeaa28945
jnccd <kobidogao@outlook.com>
2018-04-01 23:47:58 +0200

    Exporting the music library is now possible

---
+++ MusicPlayer/MusicPlayer/UpdateExports.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MusicPlayer
+{
+    partial class UpdateExports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 13);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(313, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 0;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(12, 45);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(0, 13);
+            this.progressLabel.TabIndex = 1;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // UpdateExports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 67);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(354, 106);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(354, 106);
+            this.Name = "UpdateExports";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Exporting";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UpdateMetadata_FormClosing);
+            this.Load += new System.EventHandler(this.UpdateMetadata_Load);
+            this.Shown += new System.EventHandler(this.UpdateMetadata_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progressLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ MusicPlayer/MusicPlayer/UpdateExports.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class UpdateExports : Form
+    {
+        string[] SongPaths;
+        bool closing = false;
+        string Target;
+
+        public UpdateExports(string[] SongPaths, string Target)
+        {
+            InitializeComponent();
+            this.Text = "Exporting";
+            this.SongPaths = SongPaths;
+            this.Target = Target;
+        }
+
+        private void UpdateMetadata_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateMetadata_Shown(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < SongPaths.Length; i++)
+            {
+                backgroundWorker1.ReportProgress(i);
+                string name = Path.GetFileNameWithoutExtension(SongPaths[i]);
+                try
+                {
+                    File.Copy(SongPaths[i], Target + "\\" + Path.GetFileName(SongPaths[i]));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error processing: " + name + "\nError Message: " + ex.Message);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = (int)(e.ProgressPercentage / (float)SongPaths.Length * 100);
+            progressLabel.Text = progressBar.Value + " % " + Path.GetFileNameWithoutExtension(SongPaths[e.ProgressPercentage]);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            closing = true;
+            this.Close();
+        }
+
+        private void UpdateMetadata_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (closing == false)
+                e.Cancel = true;
+        }
+    }
+}
