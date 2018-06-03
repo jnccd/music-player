30bbc1e1fab848657efccb0f107eec2f7a729538
jnccd <kobidogao@outlook.com>
2018-06-04 00:37:10 +0200

    Fixed program behavior when playing song entries in statistics that currently arent in the playlist

--- MusicPlayer/MusicPlayer/Assets.cs
+++ MusicPlayer/MusicPlayer/Assets.cs
@@ -677,12 +677,11 @@ namespace MusicPlayer
         }
         public static bool PlayPlaylistSong(string SongNameWithFileEnd)
         {
-            SaveUserSettings();
-
             for (int i = 0; i < Playlist.Count; i++)
             {
                 if (Playlist[i].Split('\\').Last() == SongNameWithFileEnd)
                 {
+                    SaveUserSettings();
                     PlayerHistory.Add(Playlist[i]);
                     PlayerHistoryIndex = PlayerHistory.Count - 1;
                     PlaySongByPath(Playlist[i]);
